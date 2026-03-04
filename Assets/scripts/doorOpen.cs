024b567b1cf3fa8debdea3440030fd21174f5f5c
ItzTyRobin <trevor.childers@vikings.berry.edu>
2026-03-04 12:53:24 -0500

    worked on door rotation and added hand models

---
+++ Assets/scripts/doorOpen.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class doorOpen : MonoBehaviour {
+
+    public Transform doorHinge;
+    public float openAngle = -90f;
+    public float speed = 3f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
